b715afa86a0f6168337cfed1553b5adac8a808ec
ShineWithoutE <wangq1998@outlook.com>
2024-12-08 22:47:12 +0800

    上传公众号创建的模板工程

---
+++ STM32F407_Template/Apps/AppMain/main.c
@@ -0,0 +1,133 @@
+/**
+ ******************************************************************************
+ * @file    Project/STM32F4xx_StdPeriph_Templates/main.c
+ * @author  MCD Application Team
+ * @version V1.8.1
+ * @date    27-January-2022
+ * @brief   Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2016 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_conf.h"
+
+/** @addtogroup Template_Project
+ * @{
+ */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t uwTimingDelay;
+RCC_ClocksTypeDef RCC_Clocks;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Delay(__IO uint32_t nTime);
+
+/* Private functions ---------------------------------------------------------*/
+
+
+void LED_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+/**
+ * @brief  Main program
+ * @param  None
+ * @retval None
+ */
+int main(void)
+{
+    /*!< At this stage the microcontroller clock setting is already configured,
+          this is done through SystemInit() function which is called from startup
+          files before to branch to application main.
+          To reconfigure the default setting of SystemInit() function,
+          refer to system_stm32f4xx.c file */
+
+    /* SysTick end of count event each 10ms */
+    RCC_GetClocksFreq(&RCC_Clocks);
+    SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+
+    LED_Init();
+
+    /* Infinite loop */
+    while (1)
+    {
+        GPIO_SetBits(GPIOB, GPIO_Pin_2);
+        Delay(50);
+
+        GPIO_ResetBits(GPIOB, GPIO_Pin_2);
+        Delay(50);
+    }
+}
+
+/**
+ * @brief  Inserts a delay time.
+ * @param  nTime: specifies the delay time length, in milliseconds.
+ * @retval None
+ */
+void Delay(__IO uint32_t nTime)
+{
+    uwTimingDelay = nTime;
+
+    while (uwTimingDelay != 0);
+}
+
+/**
+ * @brief  Decrements the TimingDelay variable.
+ * @param  None
+ * @retval None
+ */
+void TimingDelay_Decrement(void)
+{
+    if (uwTimingDelay != 0x00)
+    {
+        uwTimingDelay--;
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+    /* Infinite loop */
+    while (1)
+    {
+    }
+}
+#endif
+
+/**
+ * @}
+ */
